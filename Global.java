bbd13622593a99b6de5200f6ce867b85e0eb831c
Rave <Raven5887@gmail.com>
2012-11-17 14:00:25 +0100

    Improved Global.java

--- Global.java
+++ Global.java
@@ -4,14 +4,18 @@ public class Global extends GlobalSettings {
 
   @Override
   public void onStart(Application app) {
-    Logger.info("Application has started");
+    Logger.info("Application has started :D");
     
+    String fs = File.separator;
+    File creationsFolder = new File(fs+"public"+fs+"images"+fs+"creations");
+    
+    Logger.info("Check: "+ creationsFolder)
     
   }  
   
   @Override
   public void onStop(Application app) {
-    Logger.info("Application shutdown...");
+    Logger.info("Application shutdown D:");
   }  
     
 }
